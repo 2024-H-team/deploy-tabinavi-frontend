f039b7b638527657e7a4653da14fb989b5e83396
NGUYEN DUC HUYNH <duchuynhnguyen1906@gmail.com>
2025-01-17 17:14:28 +0900

    feat: add web app manifest and metadata for improved PWA support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,52 @@ import 'normalize.css';
 import '@styles/GlobalStyles.scss';
 import { MapProvider } from '@/components/MapProvider';
 
+import { Metadata } from 'next';
+
+export const generateMetadata = async (): Promise<Metadata> => {
+    return {
+        title: '旅ナビ',
+        description: '情報収集の負担をAIで軽減できる旅行計画Webアプリ',
+        applicationName: '旅ナビ',
+        appleWebApp: {
+            capable: true,
+            title: '情報収集の負担をAIで軽減できる旅行計画Webアプリ',
+            statusBarStyle: 'default',
+        },
+        icons: {
+            icon: [
+                {
+                    rel: 'icon',
+                    type: 'image/x-icon',
+                    sizes: '48x48',
+                    url: '/favicon.ico',
+                },
+                {
+                    rel: 'icon',
+                    type: 'image/png',
+                    sizes: '16x16',
+                    url: '/favicon-96x96.png',
+                },
+                {
+                    rel: 'icon',
+                    type: 'image/png',
+                    sizes: '32x32',
+                    url: '/favicon-96x96.png',
+                },
+                {
+                    url: '/android-chrome-192x192.png',
+                    sizes: '192x192',
+                },
+                {
+                    url: '/android-chrome-512x512.png',
+                    sizes: '512x512',
+                },
+            ],
+            apple: '/apple-touch-icon.png',
+        },
+    };
+};
+
 const inter = Inter({
     subsets: ['latin'],
     weight: ['100', '300', '400', '500', '600', '700', '800', '900'],
